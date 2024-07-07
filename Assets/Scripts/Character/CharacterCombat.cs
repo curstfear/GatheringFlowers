cb0c1cc311345bede3ef980b4509031c0d831f82
Michael <kateshii16@gmail.com>
2024-07-07 17:27:36 +0300

    Сделал кулдаун между атами

--- Assets/Scripts/Character/CharacterCombat.cs
+++ Assets/Scripts/Character/CharacterCombat.cs
@@ -9,21 +9,23 @@ public class CharacterCombat : MonoBehaviourPun
     private bool isAttacking = false;
     [SerializeField] private Transform _attackPoint;
     [SerializeField] private float _meleeAttackRange = 0.5f;
+    private bool _canAttack = true;
+    [SerializeField] private float _attackCooldown = 1f;
     public int _characterDamage = 10;
     public LayerMask _playerLayer;
 
     private void Update()
     {
-        if (photonView.IsMine && Input.GetMouseButton(0))
+        if (photonView.IsMine && Input.GetMouseButton(0) && _canAttack)
         {
             Attack();
-            
         }
     }
 
     void Attack()
     {
-
+        
+        _canAttack = false;
         // Отыграть анимацию атаки (если требуется)
         _animator.SetTrigger("Attack"); // Установить флаг, что анимация атаки запущена
 
@@ -42,6 +44,13 @@ public class CharacterCombat : MonoBehaviourPun
                 isAttacking = false; // Здесь 10 - пример количества урона
             }
         }
+        StartCoroutine(AttackCooldown());
+    }
+
+    IEnumerator AttackCooldown()
+    {
+        yield return new WaitForSeconds(_attackCooldown);
+        _canAttack = true;
     }
 
     void OnDrawGizmosSelected()
