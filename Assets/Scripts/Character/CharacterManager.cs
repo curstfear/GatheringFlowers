ec3027afb31256f885cff8565844be4ca4bee08d
Michael <kateshii16@gmail.com>
2024-07-07 12:24:36 +0300

    Еще раз пофиксил баг с здоровьем

--- Assets/Scripts/Character/CharacterManager.cs
+++ Assets/Scripts/Character/CharacterManager.cs
@@ -7,7 +7,7 @@ public class CharacterManager : MonoBehaviourPun
     private PhotonView _photonView;
     private CameraFollow _camera;
     public Text _nickName;
-    [SerializeField] private Image _HealthBackground;
+    [SerializeField] private Image _HealthFill;
     void Start()
     {
         
@@ -18,7 +18,7 @@ public class CharacterManager : MonoBehaviourPun
             _camera = Camera.main.GetComponent<CameraFollow>();
             _camera.Initialize(gameObject.transform);
             _nickName.color = Color.white;
-            _HealthBackground.color = Color.green;
+            _HealthFill.color = Color.green;
         }
 
         _nickName.text = _photonView.Owner.NickName;
